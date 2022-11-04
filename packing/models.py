6d7d60c671b97423ae14a08dcc312937de74651a
RadheyaM <radheyamansel@hotmail.com>
2022-11-04 09:50:50 +0000

    Add help text to the models, and make some other minor improvements as necessary

--- packing/models.py
+++ packing/models.py
@@ -9,11 +9,11 @@ COMPLETE = (("No", "No"), ("Yes", "Yes"))
 
 
 class Packaging(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    film = models.CharField(max_length=50)
-    inner = models.CharField(max_length=50)
-    srp = models.CharField(max_length=50, default="No SRP")
-    outer = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True, help_text="Name of the packing configuration.")
+    film = models.CharField(max_length=15, help_text="Film used to seal an inner pack.")
+    inner = models.CharField(max_length=15, help_text="The inner packet.")
+    srp = models.CharField(max_length=15, default="No SRP", help_text="Shelf Ready Pack, usually and extra 1-for-1 cover on the inner.")
+    outer = models.CharField(max_length=15, help_text="The outer box or case in which inners are packed.")
 
     def __str__(self):
         return self.name
@@ -21,28 +21,28 @@ class Packaging(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    customer = models.CharField(max_length=50)
-    packaging = models.ForeignKey(Packaging, on_delete=models.CASCADE)
-    ppt = models.IntegerField()
-    pack_sz = models.IntegerField()
-    ppc = models.IntegerField()
+    customer = models.CharField(max_length=15)
+    packaging = models.ForeignKey(Packaging, on_delete=models.CASCADE, help_text="Name of the packing configuration.")
+    ppt = models.IntegerField(help_text="Pieces(portions) Per Tray associated with a particular product.")
+    pack_sz = models.IntegerField(help_text="How many pieces fit in an inner packet.")
+    ppc = models.IntegerField(help_text="Pieces Per Case")
 
     def __str__(self):
         return self.customer + " " + self.name + " " + str(self.pack_sz) + " x " + str(self.ppc)
 
 
 class Week(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    start_date = models.DateField(auto_now=False, auto_now_add=False)
-    status = models.IntegerField(choices=PLANNING_STATUS)
+    name = models.CharField(max_length=50, unique=True, help_text="such as: October Wk2 2022, for the second week of october.")
+    start_date = models.DateField(auto_now=False, auto_now_add=False, help_text="Date the plan begins, usually a Saturday.")
+    status = models.IntegerField(choices=PLANNING_STATUS, help_text="Pre-production, production, post-production.")
 
     def __str__(self):
         return self.name
 
 
 class Run(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    case_qty = models.IntegerField()
+    name = models.CharField(max_length=50, unique=True, help_text="Product Name and case qty makes sense here.")
+    case_qty = models.IntegerField(help_text="The number of cases that need to be packed to meet orders.")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -54,7 +54,7 @@ class Packing(models.Model):
     week = models.ForeignKey(Week, on_delete=models.CASCADE)
     team = models.IntegerField(choices=TEAMS)
     day = models.CharField(max_length=15, choices=DAYS)
-    time = models.IntegerField()
+    time = models.IntegerField(help_text="Estimated time in minutes to complete the packing-run.")
     complete = models.CharField(max_length=5, choices=COMPLETE)
 
     def __str__(self):
