a3844a3c1e72c5a049d9261169808f97d59da0b5
RadheyaM <radheyamansel@hotmail.com>
2022-11-22 16:06:22 +0000

    Finish setting up django_allauth

--- planner/settings.py
+++ planner/settings.py
@@ -42,13 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
-    'django.contrib.staticfiles',
-    'cloudinary',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
     # my app
     'packing',
 ]
@@ -108,7 +108,10 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
-
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_REQUIRED = False
 
 
 # Password validation
